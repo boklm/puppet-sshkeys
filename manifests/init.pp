1bb939e872e086b7e7c76454c5649af879d2a5c5
Nicolas Vigier <boklm@hupstream.com>
2012-11-14 22:26:10 +0100

    Move setup_key_master into a separate file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,6 @@ class sshkeys {
 
   $keymaster_storage = "/var/lib/keys"
 
-  Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
   Notify { withpath => false }
 
 
@@ -45,7 +44,7 @@ class sshkeys {
       value => $_filename,
     }
 
-    @setup_key_master { $title:
+    @sshkeys::setup_key_master { $title:
       ensure  => $ensure,
       force   => $force,
       keytype => $keytype,
@@ -80,7 +79,7 @@ class sshkeys {
       mode   => 644,
     }
     # Realize all virtual master keys
-    Setup_key_master <| |>
+    Sshkeys::Setup_key_master <| |>
   }
 
 
@@ -161,91 +160,6 @@ class sshkeys {
       }
     }
     realize Ssh_auth_key_server[$title]
-  }
-}
-
-
-# Create/regenerate/remove a key pair on the keymaster.
-# This definition is private, i.e. it is not intended to be called directly by users.
-# sshkeys::key calls it to create virtual keys, which are realized in sshkeys::keymaster.
-define setup_key_master (
-  $ensure,
-  $force,
-  $keytype,
-  $length,
-  $maxdays,
-  $mindate
-) {
-  Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
-  File {
-    owner => puppet,
-    group => puppet,
-    mode  => 600,
-  }
-
-  $keydir = "${sshkeys::keymaster_storage}/${title}"
-  $keyfile = "${keydir}/key"
-
-  file {
-    "$keydir":
-      ensure => directory,
-      mode   => 644;
-    "$keyfile":
-      ensure => $ensure;
-    "${keyfile}.pub":
-      ensure => $ensure,
-      mode   => 644;
-  }
-
-  if $ensure == "present" {
-
-    # Remove the existing key pair, if
-    # * $force is true, or
-    # * $maxdays or $mindate criteria aren't met, or
-    # * $keytype or $length have changed
-
-    $keycontent = file("${keyfile}.pub", "/dev/null")
-    if $keycontent {
-
-      if $force {
-        $reason = "force=true"
-      }
-      if !$reason and $mindate and generate("/usr/bin/find", $keyfile, "!", "-newermt", "${mindate}") {
-        $reason = "created before ${mindate}"
-      }
-      if !$reason and $maxdays and generate("/usr/bin/find", $keyfile, "-mtime", "+${maxdays}") {
-        $reason = "older than ${maxdays} days"
-      }
-      if !$reason and $keycontent =~ /^ssh-... [^ ]+ (...) (\d+)$/ {
-        if $keytype != $1 {
-	  $reason = "keytype changed: $1 -> $keytype"
-	} else {
-	  if $length != $2 {
-	    $reason = "length changed: $2 -> $length"
-	  }
-	}
-      }
-      if $reason {
-        exec { "Revoke previous key ${title}: ${reason}":
-          command => "rm $keyfile ${keyfile}.pub",
-          before  => Exec["Create key $title: $keytype, $length bits"],
-        }
-      }
-    }
-
-    # Create the key pair.
-    # We "repurpose" the comment field in public keys on the keymaster to
-    # store data about the key, i.e. $keytype and $length.  This avoids
-    # having to rerun ssh-keygen -l on every key at every run to determine
-    # the key length.
-    exec { "Create key $title: $keytype, $length bits":
-      command => "ssh-keygen -t ${keytype} -b ${length} -f ${keyfile} -C \"${keytype} ${length}\" -N \"\"",
-      user    => "puppet",
-      group   => "puppet",
-      creates => $keyfile,
-      require => File[$keydir],
-      before  => File[$keyfile, "${keyfile}.pub"],
-    }
   }
 }
 
