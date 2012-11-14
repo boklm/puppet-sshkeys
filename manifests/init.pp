98ca34574182e56e8b465cbaa2171687c0b459a2
Nicolas Vigier <boklm@hupstream.com>
2012-11-14 19:38:10 +0100

    reindent

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,137 +10,202 @@
 
 class ssh::auth {
 
-$keymaster_storage = "/var/lib/keys"
+  $keymaster_storage = "/var/lib/keys"
 
-Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
-Notify { withpath => false }
-
-
-##########################################################################
+  Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
+  Notify { withpath => false }
 
 
-# ssh::auth::key
-
-# Declare keys.  The approach here is just to define a bunch of
-# virtual resources, representing key files on the keymaster, client,
-# and server.  The virtual keys are then realized by
-# ssh::auth::{keymaster,client,server}, respectively.  The reason for
-# doing things that way is that it makes ssh::auth::key into a "one
-# stop shop" where users can declare their keys with all of their
-# parameters, whether those parameters apply to the keymaster, server,
-# or client.  The real work of creating, installing, and removing keys
-# is done in the private definitions called by the virtual resources:
-# ssh_auth_key_{master,server,client}.
-
-define key ($ensure = "present", $filename = "", $force = false, $group = "puppet", $home = "", $keytype = "rsa", $length = 2048, $maxdays = "", $mindate = "", $options = "", $user = "") {
-
-  ssh_auth_key_namecheck { "${title}-title": parm => "title", value => $title }
-
-  # apply defaults
-  $_filename = $filename ? { "" => "id_${keytype}", default => $filename }
-  $_length = $keytype ? { "rsa" => $length, "dsa" => 1024 }
-  $_user = $user ? {
-    ""      => regsubst($title, '^([^@]*)@?.*$', '\1'),
-    default => $user,
-  }
-  $_home = $home ? { "" => "/home/$_user",  default => $home }
-
-  ssh_auth_key_namecheck { "${title}-filename": parm => "filename", value => $_filename }
-
-  @ssh_auth_key_master { $title:
-    ensure  => $ensure,
-    force   => $force,
-    keytype => $keytype,
-    length  => $_length,
-    maxdays => $maxdays,
-    mindate => $mindate,
-  }
-  @ssh_auth_key_client { $title:
-    ensure   => $ensure,
-    filename => $_filename,
-    group    => $group,
-    home     => $_home,
-    user     => $_user,
-  }
-  @ssh_auth_key_server { $title:
-    ensure  => $ensure,
-    group   => $group,
-    home    => $_home,
-    options => $options,
-    user    => $_user,
-  }
-}
+  ##########################################################################
 
 
-##########################################################################
+  # ssh::auth::key
 
+  # Declare keys.  The approach here is just to define a bunch of
+  # virtual resources, representing key files on the keymaster, client,
+  # and server.  The virtual keys are then realized by
+  # ssh::auth::{keymaster,client,server}, respectively.  The reason for
+  # doing things that way is that it makes ssh::auth::key into a "one
+  # stop shop" where users can declare their keys with all of their
+  # parameters, whether those parameters apply to the keymaster, server,
+  # or client.  The real work of creating, installing, and removing keys
+  # is done in the private definitions called by the virtual resources:
+  # ssh_auth_key_{master,server,client}.
 
-# ssh::auth::keymaster
-#
-# Keymaster host:
-# Create key storage; create, regenerate, and remove key pairs
+  define key (
+    $ensure = "present",
+    $filename = "",
+    $force = false,
+    $group = "puppet",
+    $home = "",
+    $keytype = "rsa",
+    $length = 2048,
+    $maxdays = "",
+    $mindate = "",
+    $options = "",
+    $user = ""
+  ) {
+    ssh_auth_key_namecheck { "${title}-title": parm => "title", value => $title }
 
-class keymaster {
+    # apply defaults
+    $_filename = $filename ? { "" => "id_${keytype}", default => $filename }
+    $_length = $keytype ? { "rsa" => $length, "dsa" => 1024 }
+    $_user = $user ? {
+      ""      => regsubst($title, '^([^@]*)@?.*$', '\1'),
+      default => $user,
+    }
+    $_home = $home ? { "" => "/home/$_user",  default => $home }
 
-  # Set up key storage
+    ssh_auth_key_namecheck { "${title}-filename":
+      parm => "filename",
+      value => $_filename,
+    }
 
-  file { $ssh::auth::keymaster_storage:
-    ensure => directory,
-    owner  => puppet,
-    group  => puppet,
-    mode   => 644,
+    @ssh_auth_key_master { $title:
+      ensure  => $ensure,
+      force   => $force,
+      keytype => $keytype,
+      length  => $_length,
+      maxdays => $maxdays,
+      mindate => $mindate,
+    }
+    @ssh_auth_key_client { $title:
+      ensure   => $ensure,
+      filename => $_filename,
+      group    => $group,
+      home     => $_home,
+      user     => $_user,
+    }
+    @ssh_auth_key_server { $title:
+      ensure  => $ensure,
+      group   => $group,
+      home    => $_home,
+      options => $options,
+      user    => $_user,
+    }
   }
 
-  # Realize all virtual master keys
-  Ssh_auth_key_master <| |>
 
-} # class keymaster
+  ##########################################################################
 
 
-##########################################################################
+  # ssh::auth::keymaster
+  #
+  # Keymaster host:
+  # Create key storage; create, regenerate, and remove key pairs
+
+  class keymaster {
+
+    # Set up key storage
+
+    file { $ssh::auth::keymaster_storage:
+      ensure => directory,
+      owner  => puppet,
+      group  => puppet,
+      mode   => 644,
+    }
+
+    # Realize all virtual master keys
+    Ssh_auth_key_master <| |>
+
+  } # class keymaster
 
 
-# ssh::auth::client
-#
-# Install generated key pairs onto clients
-
-define client ($ensure = "", $filename = "", $group = "", $home = "", $user = "") {
-
-  # Realize the virtual client keys.
-  # Override the defaults set in ssh::auth::key, as needed.
-  if $ensure   { Ssh_auth_key_client <| title == $title |> { ensure   => $ensure   } }
-  if $filename { Ssh_auth_key_client <| title == $title |> { filename => $filename } }
-  if $group    { Ssh_auth_key_client <| title == $title |> { group    => $group    } }
-
-  if $user { Ssh_auth_key_client <| title == $title |> { user => $user, home => "/home/$user" } }
-  if $home { Ssh_auth_key_client <| title == $title |> { home => $home } }
-
-  realize Ssh_auth_key_client[$title]
-
-} # define client
+  ##########################################################################
 
 
-##########################################################################
+  # ssh::auth::client
+  #
+  # Install generated key pairs onto clients
+
+  define client (
+    $ensure = "",
+    $filename = "",
+    $group = "",
+    $home = "",
+    $user = ""
+  ) {
+    # Realize the virtual client keys.
+    # Override the defaults set in ssh::auth::key, as needed.
+    if $ensure {
+      Ssh_auth_key_client <| title == $title |> {
+	ensure   => $ensure,
+      }
+    }
+    if $filename {
+      Ssh_auth_key_client <| title == $title |> {
+	filename => $filename,
+      }
+    }
+    if $group {
+      Ssh_auth_key_client <| title == $title |> {
+	group    => $group,
+      }
+    }
+
+    if $user {
+      Ssh_auth_key_client <| title == $title |> {
+	user => $user,
+	home => "/home/$user",
+      }
+    }
+    if $home {
+      Ssh_auth_key_client <| title == $title |> {
+	home => $home
+      }
+    }
+
+    realize Ssh_auth_key_client[$title]
+
+  } # define client
 
 
-# ssh::auth::server
-#
-# Install public keys onto clients
+  ##########################################################################
 
-define server ($ensure = "", $group = "", $home = "", $options = "", $user = "") {
 
-  # Realize the virtual server keys.
-  # Override the defaults set in ssh::auth::key, as needed.
-  if $ensure  { Ssh_auth_key_server <| title == $title |> { ensure  => $ensure  } }
-  if $group   { Ssh_auth_key_server <| title == $title |> { group   => $group   } }
-  if $options { Ssh_auth_key_server <| title == $title |> { options => $options } }
+  # ssh::auth::server
+  #
+  # Install public keys onto clients
 
-  if $user { Ssh_auth_key_server <| title == $title |> { user => $user, home => "/home/$user" } }
-  if $home { Ssh_auth_key_server <| title == $title |> { home => $home } }
+  define server (
+    $ensure = "",
+    $group = "",
+    $home = "",
+    $options = "",
+    $user = ""
+  ) {
+    # Realize the virtual server keys.
+    # Override the defaults set in ssh::auth::key, as needed.
+    if $ensure {
+      Ssh_auth_key_server <| title == $title |> {
+	ensure  => $ensure,
+      }
+    }
+    if $group {
+      Ssh_auth_key_server <| title == $title |> {
+	group   => $group,
+      }
+    }
+    if $options {
+      Ssh_auth_key_server <| title == $title |> {
+	options => $options,
+      }
+    }
 
-  realize Ssh_auth_key_server[$title]
+    if $user {
+      Ssh_auth_key_server <| title == $title |> {
+	user => $user, home => "/home/$user",
+      }
+    }
+    if $home {
+      Ssh_auth_key_server <| title == $title |> {
+	home => $home,
+      }
+    }
 
-} # define server
+    realize Ssh_auth_key_server[$title]
+
+  } # define server
 
 } # class ssh::auth
 
@@ -154,8 +219,14 @@ define server ($ensure = "", $group = "", $home = "", $options = "", $user = "")
 # This definition is private, i.e. it is not intended to be called directly by users.
 # ssh::auth::key calls it to create virtual keys, which are realized in ssh::auth::keymaster.
 
-define ssh_auth_key_master ($ensure, $force, $keytype, $length, $maxdays, $mindate) {
-
+define ssh_auth_key_master (
+  $ensure,
+  $force,
+  $keytype,
+  $length,
+  $maxdays,
+  $mindate
+) {
   Exec { path => "/usr/bin:/usr/sbin:/bin:/sbin" }
   File {
     owner => puppet,
@@ -197,8 +268,13 @@ define ssh_auth_key_master ($ensure, $force, $keytype, $length, $maxdays, $minda
         $reason = "older than ${maxdays} days"
       }
       if !$reason and $keycontent =~ /^ssh-... [^ ]+ (...) (\d+)$/ {
-        if       $keytype != $1 { $reason = "keytype changed: $1 -> $keytype" }
-        else { if $length != $2 { $reason = "length changed: $2 -> $length" } }
+        if $keytype != $1 {
+	  $reason = "keytype changed: $1 -> $keytype"
+	} else {
+	  if $length != $2 {
+	    $reason = "length changed: $2 -> $length"
+	  }
+	}
       }
       if $reason {
         exec { "Revoke previous key ${title}: ${reason}":
@@ -235,8 +311,13 @@ define ssh_auth_key_master ($ensure, $force, $keytype, $length, $maxdays, $minda
 # Install a key pair into a user's account.
 # This definition is private, i.e. it is not intended to be called directly by users.
 
-define ssh_auth_key_client ($ensure, $filename, $group, $home, $user) {
-
+define ssh_auth_key_client (
+  $ensure,
+  $filename,
+  $group,
+  $home,
+  $user
+) {
   File {
     owner   => $user,
     group   => $group,
@@ -275,8 +356,13 @@ define ssh_auth_key_client ($ensure, $filename, $group, $home, $user) {
 # Install a public key into a server user's authorized_keys(5) file.
 # This definition is private, i.e. it is not intended to be called directly by users.
 
-define ssh_auth_key_server ($ensure, $group, $home, $options, $user) {
-
+define ssh_auth_key_server (
+  $ensure,
+  $group,
+  $home,
+  $options,
+  $user
+) {
   # on the keymaster:
   $key_src_dir = "${ssh::auth::keymaster_storage}/${title}"
   $key_src_file = "${key_src_dir}/key.pub"
@@ -295,25 +381,33 @@ define ssh_auth_key_server ($ensure, $group, $home, $options, $user) {
   }
 
   if $ensure == "absent" {
-    ssh_authorized_key { $title: ensure => "absent" }
+    ssh_authorized_key { $title:
+      ensure => "absent",
+    }
   }
   else {
     $key_src_content = file($key_src_file, "/dev/null")
     if ! $key_src_content {
-      notify { "Public key file $key_src_file for key $title not found on keymaster; skipping ensure => present": }
-    } else { if $ensure == "present" and $key_src_content !~ /^(ssh-...) ([^ ]*)/ {
-      err("Can't parse public key file $key_src_file")
-      notify { "Can't parse public key file $key_src_file for key $title on the keymaster: skipping ensure => $ensure": }
-    } else {
-      $keytype = $1
-      $modulus = $2
-      ssh_authorized_key { $title:
-        ensure  => "present",
-        type    => $keytype,
-        key     => $modulus,
-        options => $options ? { "" => undef, default => $options },
+      notify {
+	"Public key file $key_src_file for key $title not found on keymaster; skipping ensure => present":
       }
-    }} # if ... else ... else
+    } else {
+      if $ensure == "present" and $key_src_content !~ /^(ssh-...) ([^ ]*)/ {
+	err("Can't parse public key file $key_src_file")
+	notify {
+	  "Can't parse public key file $key_src_file for key $title on the keymaster: skipping ensure => $ensure":
+	}
+      } else {
+	$keytype = $1
+	$modulus = $2
+	ssh_authorized_key { $title:
+	  ensure  => "present",
+	  type    => $keytype,
+	  key     => $modulus,
+	  options => $options ? { "" => undef, default => $options },
+	}
+      }
+    } # if ... else ... else
   } # if ... else
 
 } # define ssh_auth_key_server
@@ -326,7 +420,10 @@ define ssh_auth_key_server ($ensure, $group, $home, $options, $user) {
 #
 # Check a name (e.g. key title or filename) for the allowed form
 
-define ssh_auth_key_namecheck ($parm, $value) {
+define ssh_auth_key_namecheck (
+  $parm,
+  $value
+) {
   if $value !~ /^[A-Za-z0-9]/ {
     fail("ssh::auth::key: $parm '$value' not allowed: must begin with a letter or digit")
   }
