fe16e2e1d916766c37b1a46169049aebe7a2db4a
Nicolas Vigier <boklm@hupstream.com>
2012-11-15 18:08:13 +0100

    Move install_client_key_pair into a separate file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,46 +2,6 @@ class sshkeys {
 
   $keymaster_storage = "/var/lib/keys"
 
-  # Install generated key pairs onto clients
-  define install_client_key_pair (
-    $ensure = "",
-    $filename = "",
-    $group = "",
-    $home = "",
-    $user = ""
-  ) {
-    # Realize the virtual client keys.
-    # Override the defaults set in sshkeys::key, as needed.
-    if $ensure {
-      Sshkeys::Setup_client_key_pair <| title == $title |> {
-	ensure   => $ensure,
-      }
-    }
-    if $filename {
-      Sshkeys::Setup_client_key_pair <| title == $title |> {
-	filename => $filename,
-      }
-    }
-    if $group {
-      Sshkeys::Setup_client_key_pair <| title == $title |> {
-	group    => $group,
-      }
-    }
-    if $user {
-      Sshkeys::Setup_client_key_pair <| title == $title |> {
-	user => $user,
-	home => "/home/$user",
-      }
-    }
-    if $home {
-      Sshkeys::Setup_client_key_pair <| title == $title |> {
-	home => $home
-      }
-    }
-    realize Sshkeys::Setup_client_key_pair[$title]
-  }
-
-
   # Install public keys onto clients
   define server (
     $ensure = "",
