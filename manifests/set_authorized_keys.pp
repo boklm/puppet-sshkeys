0a3b1285a787b18f1ee05c05cd89b11d7e54e428
Nicolas Vigier <boklm@hupstream.com>
2012-11-15 23:24:23 +0100

    set_authorized_keys: add keyname parameter
    Add a parameter so that it's possible to use a key different from title.
    If $keyname is not set, use $title as key name.
    
    This permit installing the same key on multiple authorized_keys files.

--- manifests/set_authorized_keys.pp
+++ manifests/set_authorized_keys.pp
@@ -2,15 +2,17 @@ Notify { withpath => false }
 
 # Install a public key into a server user's authorized_keys(5) file.
 define sshkeys::set_authorized_keys (
+  $keyname = '',
   $ensure = 'present',
   $group = '',
   $home = '',
   $options = '',
   $user
 ) {
+  $_keyname = $keyname ? { '' => $title, default => $keyname }
   $_home = $home ? { "" => "/home/${user}", default => $home }
   # on the keymaster:
-  $key_src_dir = "${sshkeys::keymaster_storage}/${title}"
+  $key_src_dir = "${sshkeys::keymaster_storage}/${_keyname}"
   $key_src_file = "${key_src_dir}/key.pub"
   # on the server:
   $key_tgt_file = "${_home}/.ssh/authorized_keys"
@@ -34,13 +36,13 @@ define sshkeys::set_authorized_keys (
     $key_src_content = file($key_src_file, "/dev/null")
     if ! $key_src_content {
       notify {
-	"Public key file $key_src_file for key $title not found on keymaster; skipping ensure => present":
+	"Public key file $key_src_file for key $_keyname not found on keymaster; skipping ensure => present":
       }
     } else {
       if $ensure == "present" and $key_src_content !~ /^(ssh-...) ([^ ]*)/ {
 	err("Can't parse public key file $key_src_file")
 	notify {
-	  "Can't parse public key file $key_src_file for key $title on the keymaster: skipping ensure => $ensure":
+	  "Can't parse public key file $key_src_file for key $_keyname on the keymaster: skipping ensure => $ensure":
 	}
       } else {
 	$keytype = $1
